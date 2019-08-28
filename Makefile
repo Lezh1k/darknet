5cf4cba16062d9415b6b06caa94da12711c7f781
Lezh1k <lezh1k.vohrer@gmail.com>
2019-08-28 12:19:55 +0600

    Opencv4 support

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 GPU=0
 CUDNN=0
-OPENCV=0
+OPENCV=1
 OPENMP=0
 DEBUG=0
 
@@ -40,10 +40,10 @@ endif
 CFLAGS+=$(OPTS)
 
 ifeq ($(OPENCV), 1) 
-COMMON+= -DOPENCV
+COMMON+= -DOPENCV -I/usr/include/opencv4 -I/usr/include/opencv4/opencv2
 CFLAGS+= -DOPENCV
-LDFLAGS+= `pkg-config --libs opencv` -lstdc++
-COMMON+= `pkg-config --cflags opencv` 
+LDFLAGS+= `pkg-config --libs opencv4` -lstdc++
+COMMON+= `pkg-config --cflags opencv4` 
 endif
 
 ifeq ($(GPU), 1) 
--- src/image_opencv.cpp
+++ src/image_opencv.cpp
@@ -4,130 +4,132 @@
 #include "stdlib.h"
 #include "opencv2/opencv.hpp"
 #include "image.h"
+#include "assert.h"
 
 using namespace cv;
 
 extern "C" {
 
-IplImage *image_to_ipl(image im)
+static int TypeFromChannelsCount(int cn) {
+  assert(cn >= 1 && cn <= 4);
+  int types[4] = {CV_8UC1, CV_8UC2, CV_8UC3, CV_8UC4};
+  return types[cn-1];
+}
+
+static Mat image_to_mat_internal(image im)
 {
-    int x,y,c;
-    IplImage *disp = cvCreateImage(cvSize(im.w,im.h), IPL_DEPTH_8U, im.c);
-    int step = disp->widthStep;
-    for(y = 0; y < im.h; ++y){
-        for(x = 0; x < im.w; ++x){
-            for(c= 0; c < im.c; ++c){
-                float val = im.data[c*im.h*im.w + y*im.w + x];
-                disp->imageData[y*step + x*im.c + c] = (unsigned char)(val*255);
-            }
-        }
+  Mat m(Size(im.w, im.h), TypeFromChannelsCount(im.c));
+  size_t step = static_cast<size_t>(m.step);
+  for(int y = 0; y < im.h; ++y){
+    for(int x = 0; x < im.w; ++x){
+      for(int c= 0; c < im.c; ++c){
+        float val = im.data[c*im.h*im.w + y*im.w + x];
+        m.data[y*step + x*im.c + c] = (unsigned char)(val*255);
+      }
     }
-    return disp;
+  }
+  return m;
 }
 
-image ipl_to_image(IplImage* src)
+static image mat_to_image_internal(const Mat& src)
 {
-    int h = src->height;
-    int w = src->width;
-    int c = src->nChannels;
-    image im = make_image(w, h, c);
-    unsigned char *data = (unsigned char *)src->imageData;
-    int step = src->widthStep;
-    int i, j, k;
+  int h = src.rows;
+  int w = src.cols;
+  int c = src.channels();
+  image im = make_image(w, h, c);
+  unsigned char *data = (unsigned char *)src.data;
+  size_t step = static_cast<size_t>(src.step);
+  int i, j, k;
 
-    for(i = 0; i < h; ++i){
-        for(k= 0; k < c; ++k){
-            for(j = 0; j < w; ++j){
-                im.data[k*w*h + i*w + j] = data[i*step + j*c + k]/255.;
-            }
-        }
+  for(i = 0; i < h; ++i){
+    for(k= 0; k < c; ++k){
+      for(j = 0; j < w; ++j){
+        im.data[k*w*h + i*w + j] = data[i*step + j*c + k]/255.;
+      }
     }
-    return im;
+  }
+  return im;
 }
 
-Mat image_to_mat(image im)
+static Mat image_to_mat(image im)
 {
-    image copy = copy_image(im);
-    constrain_image(copy);
-    if(im.c == 3) rgbgr_image(copy);
-
-    IplImage *ipl = image_to_ipl(copy);
-    Mat m = cvarrToMat(ipl, true);
-    cvReleaseImage(&ipl);
-    free_image(copy);
-    return m;
+  image copy = copy_image(im);
+  constrain_image(copy);
+  if(im.c == 3) rgbgr_image(copy);
+  Mat m = image_to_mat_internal(copy);
+  free_image(copy);
+  return m;
 }
 
-image mat_to_image(Mat m)
-{
-    IplImage ipl = m;
-    image im = ipl_to_image(&ipl);
-    rgbgr_image(im);
-    return im;
+static image mat_to_image(const Mat& m)
+{  
+  image im = mat_to_image_internal(m);
+  rgbgr_image(im);
+  return im;
 }
 
 void *open_video_stream(const char *f, int c, int w, int h, int fps)
 {
-    VideoCapture *cap;
-    if(f) cap = new VideoCapture(f);
-    else cap = new VideoCapture(c);
-    if(!cap->isOpened()) return 0;
-    if(w) cap->set(CV_CAP_PROP_FRAME_WIDTH, w);
-    if(h) cap->set(CV_CAP_PROP_FRAME_HEIGHT, w);
-    if(fps) cap->set(CV_CAP_PROP_FPS, w);
-    return (void *) cap;
+  VideoCapture *cap;
+  if(f) cap = new VideoCapture(f);
+  else cap = new VideoCapture(c);
+  if(!cap->isOpened()) return 0;
+  if(w) cap->set(CAP_PROP_FRAME_WIDTH, w);
+  if(h) cap->set(CAP_PROP_FRAME_HEIGHT, h);
+  if(fps) cap->set(CAP_PROP_FPS, w);
+  return (void *) cap;
 }
 
 image get_image_from_stream(void *p)
 {
-    VideoCapture *cap = (VideoCapture *)p;
-    Mat m;
-    *cap >> m;
-    if(m.empty()) return make_empty_image(0,0,0);
-    return mat_to_image(m);
+  VideoCapture *cap = (VideoCapture *)p;
+  Mat m;
+  *cap >> m;
+  if(m.empty()) return make_empty_image(0,0,0);
+  return mat_to_image(m);
 }
 
 image load_image_cv(char *filename, int channels)
 {
-    int flag = -1;
-    if (channels == 0) flag = -1;
-    else if (channels == 1) flag = 0;
-    else if (channels == 3) flag = 1;
-    else {
-        fprintf(stderr, "OpenCV can't force load with %d channels\n", channels);
-    }
-    Mat m;
-    m = imread(filename, flag);
-    if(!m.data){
-        fprintf(stderr, "Cannot load image \"%s\"\n", filename);
-        char buff[256];
-        sprintf(buff, "echo %s >> bad.list", filename);
-        system(buff);
-        return make_image(10,10,3);
-        //exit(0);
-    }
-    image im = mat_to_image(m);
-    return im;
+  int flag = IMREAD_UNCHANGED;
+  if (channels == 0) flag = IMREAD_UNCHANGED;
+  else if (channels == 1) flag = IMREAD_GRAYSCALE;
+  else if (channels == 3) flag = IMREAD_COLOR;
+  else {
+    fprintf(stderr, "OpenCV can't force load with %d channels\n", channels);
+  }
+  Mat m;
+  m = imread(filename, flag);
+  if(!m.data){
+    fprintf(stderr, "Cannot load image \"%s\"\n", filename);
+    char buff[256];
+    sprintf(buff, "echo %s >> bad.list", filename);
+    system(buff);
+    return make_image(10,10,3);
+    //exit(0);
+  }
+  image im = mat_to_image(m);
+  return im;
 }
 
 int show_image_cv(image im, const char* name, int ms)
 {
-    Mat m = image_to_mat(im);
-    imshow(name, m);
-    int c = waitKey(ms);
-    if (c != -1) c = c%256;
-    return c;
+  Mat m = image_to_mat(im);
+  imshow(name, m);
+  int c = waitKey(ms);
+  if (c != -1) c = c%256;
+  return c;
 }
 
 void make_window(char *name, int w, int h, int fullscreen)
 {
-    namedWindow(name, WINDOW_NORMAL); 
-    if (fullscreen) {
-        setWindowProperty(name, CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
-    } else {
-        resizeWindow(name, w, h);
-        if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
-    }
+  namedWindow(name, WINDOW_NORMAL);
+  if (fullscreen) {
+    setWindowProperty(name, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
+  } else {
+    resizeWindow(name, w, h);
+    if(strcmp(name, "Demo") == 0) moveWindow(name, 0, 0);
+  }
 }
 
 }
